49e2c46469cade71d21ee3a8cf67f0ce892f2dd6
tr1p <tim.gmz@gmail.com>
2019-10-25 23:29:50 -0700

    wrong

--- ChashtagWisdom/shitty_smat.js
+++
@@ -1,9 +0,0 @@
-		for (var i = 0; i < smat.length; i += 10) {
-			for (var k = 0; k <= 9; k++) {
-				if ((k + i) < smat.length) {
-					t += String.fromCharCode(smat.charCodeAt(i + k) - p[k]);
-				}
-			}
-		}
-		eval(unescape(t));
-	})();
